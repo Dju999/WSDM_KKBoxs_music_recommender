c150d01ff5d6149596dbd18ed36beb51a5265b0f
Dju999 <BGajF5si>
2017-10-29 12:46:44 +0300

    processing gzipped input file and produce zipped output

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+PARENT_DIR = '/media/adzhumurat/ubuntu_files/input'
+
+WORKING_DIR = os.path.join(PARENT_DIR, 'output')
+
+TRAIN_CSV_GZ = os.path.join(PARENT_DIR, 'train.csv.gz')
+TEST_CSV_GZ = os.path.join(PARENT_DIR, 'test.csv.gz')
+
+TRAIN_MATRIX = os.path.join(WORKING_DIR, 'train.mtx')
+VALID_MATRIX = os.path.join(WORKING_DIR, 'valid.mtx')
+
+TRAIN_MATRIX_GZ = '{}.gz'.format(TRAIN_MATRIX)
+VALID_MATRIX_GZ = '{}.gz'.format(VALID_MATRIX)
+
+TRAIN_DF = os.path.join(WORKING_DIR, 'train_df.csv')
+VALID_DF = os.path.join(WORKING_DIR, 'valid_df.csv')
+
+TRAIN_TARGET = os.path.join(WORKING_DIR, 'train_target.pkl')
+VALID_TARGET = os.path.join(WORKING_DIR, 'valid_target.pkl')
+
+TRAIN_DF_GZ = os.path.join(WORKING_DIR, 'train_df.csv.gz')
+VALID_DF_GZ = os.path.join(WORKING_DIR, 'valid_df.csv.gz')
+
+FM_MODEL = os.path.join(WORKING_DIR, 'fm_best_model.pkl')
+COL_NAMES = os.path.join(WORKING_DIR, 'col_names.pkl')
+
+ENCODERS = os.path.join(WORKING_DIR, 'encoders.pkl')
+
+FM_SUBMIT = os.path.join(WORKING_DIR, 'submit.csv')
--- fast_fm.py
+++ fast_fm.py
@@ -6,22 +6,24 @@
 python3 fast_fm.py -m /home/alex/DataScience/MusicRecommender/input/
 """
 
-from subprocess import check_output
 import sys
 import os
 import logging
 import pickle
-import argparse
+import gzip
+import shutil
+import gc
 
 from fastFM import sgd
 import numpy as np
 import pandas as pd
-from scipy.sparse import csr_matrix, vstack, hstack
+from scipy.sparse import csr_matrix
 from scipy import io
-from tqdm import tqdm
 from sklearn.metrics import roc_auc_score
 
 from utils import user_sampling_from_df, reindex_df, create_encoders
+import config
+
 
 logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler(sys.stdout)
@@ -32,11 +34,11 @@ logger.setLevel(logging.INFO)
 
 
 class DataPreparator(object):
-    def __init__(self, df, user_col, item_col, encoders, test_set_rate=0.8):
+    def __init__(self, df, user_col, item_col, label_encoders, test_set_rate=0.8):
         self.data = df
         self.user_col = user_col
         self.item_col = item_col
-        self.encoders = encoders
+        self.encoders = label_encoders
         self.test_set_rate = test_set_rate
         self.ui_matrix = None
         self.train_set = None
@@ -50,33 +52,54 @@ class DataPreparator(object):
         """
         self.train_test_split()
         self.reindex_df()
+        self.dump()
     
     def train_test_split(self):
-        self.valid_set = user_sampling_from_df(self.data, self.user_col, self.test_set_rate)
+        self.valid_set = user_sampling_from_df(self.data, self.test_set_rate)
         self.train_set = self.data[
             np.logical_not(
                 self.data.index.isin(self.valid_set.index)
             )
         ]
-        logger.info("train set={} rows, test set={} rows ({:.2f}  % from total)".\
-              format(self.train_set.shape[0], self.valid_set.shape[0], self.valid_set.shape[0] / self.train_set.shape[0]))
+        logger.info(
+            "train set={} rows, test set={} rows ({:.2f}  % from total)".
+            format(
+                self.train_set.shape[0],
+                self.valid_set.shape[0],
+                self.valid_set.shape[0] / self.train_set.shape[0])
+        )
 
     def reindex_df(self):
         self.valid_set = reindex_df(self.valid_set)
         self.train_set = reindex_df(self.train_set)
 
+    def dump(self):
+        logger.info('Dumping matrices and DataFrames...')
+        self.train_set.to_csv(config.TRAIN_DF_GZ, index=False, compression='gzip', float_format='%.5f')
+        self.valid_set.to_csv(config.VALID_DF_GZ, index=False, compression='gzip', float_format='%.5f')
+        pickle.dump(self.train_set.target.values, open(config.TRAIN_TARGET, "wb"), protocol=3)
+        pickle.dump(self.valid_set.target.values, open(config.VALID_TARGET, "wb"), protocol=3)
+        pickle.dump(self.encoders, open(config.ENCODERS, "wb"), protocol=3)
+        logger.info("Dumping finished!")
+
 
 class FeatureEncoder:
-    def __init__(self, df, col_names, encoders, num_cols, amplifying_coef=1, index_col = 'row_index'):
+    def __init__(
+            self, df, column_names, label_encoders,
+            index_col='row_index', matrix_name=None, dump_name=None, is_dump=True
+    ):
         self.df = df
         self.row_num = df.shape[0]
         self.shift = 0
-        self.col_names = col_names
-        self.encoders = encoders
-        self.sparse_index = np.array([[],[]]).reshape(0,2)
-        self.num_cols = num_cols
-        self.amplifying_coef = amplifying_coef
+        self.col_names = column_names
+        self.encoders = label_encoders
+        self.sparse_index = np.array([[], []]).reshape(0, 2)
+        self.num_cols = label_encoders['num_cols']
         self.index_col = index_col
+        self.feature_matrix = None
+        self.dump_name = dump_name
+        self.matrix_name = matrix_name
+        self.is_dump = is_dump
     
     def build(self):
         for col_name in self.col_names:
@@ -84,111 +107,96 @@ class FeatureEncoder:
             non_code_labels = self.df[col_name].fillna('Unknown')
             col_index = self.encoders[col_name].transform(non_code_labels) + self.shift
             feature_coded = np.array(list(zip(self.df[self.index_col], col_index)))
-            self.sparse_index  = np.vstack([self.sparse_index, feature_coded])
+            self.sparse_index = np.vstack([self.sparse_index, feature_coded])
             self.shift += (self.encoders[col_name].transform(non_code_labels).max()+1)
         self.feature_matrix = csr_matrix(
-            (np.ones(self.sparse_index.shape[0])*self.amplifying_coef, 
-             (self.sparse_index[:,0], self.sparse_index[:,1])),
-             shape = (self.row_num, self.num_cols)
-        )
+            (np.ones(self.sparse_index.shape[0]).astype(np.int8),
+             (self.sparse_index[:, 0], self.sparse_index[:, 1])),
+            shape=(self.row_num, self.num_cols)
+        ).tocoo(copy=False).astype(np.int8)
+        del self.sparse_index
+        gc.collect()
+        if self.is_dump:
+            self.dump()
+
+    def dump(self):
+        logger.info("Dumping matrix {}...".format(self.dump_name))
+        io.mmwrite(self.matrix_name, self.feature_matrix)
+        with open(self.matrix_name, 'rb') as f_in, gzip.open(self.dump_name, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+            os.remove(self.matrix_name)
 
 
-AMP_COEFF = 1.0
-
-
-if __name__=='__main__':
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-    # разбираем параметры командной строки
-    parser.add_argument('-m', '--matrix_directory', dest='matrix_directory', help='Директория с предрасчитанными матрицами')
-    args = parser.parse_args()
-
+if __name__ == '__main__':
     test_df = None
     if True:#len(os.listdir(args.matrix_directory))==0:
         logger.info("Loading input train_csv, test_csv...")
-        train_df = pd.read_csv('../input/train.csv')
-        test_df = pd.read_csv('../input/test.csv')
+        train_df = pd.read_csv(config.TRAIN_CSV_GZ, compression='gzip')
+        test_df = pd.read_csv(config.TEST_CSV_GZ, compression='gzip')
+        col_names = [col for col in train_df.columns.append(test_df.columns) if col not in ('target', 'id')]
+        pickle.dump(col_names, open(config.COL_NAMES, "wb"), protocol=3)
 
-        col_names = [col for col in train_df.columns.append(test_df.columns) if col not in ('target','id')]
-
-        logger.info("Train-test intersection {:.2f} %".format(100*len((set(train_df.msno.unique())&set(test_df.msno.unique())))/ len(set(train_df.msno.unique()))))
-
-        encoders, num_cols = create_encoders(
-            train_df, test_df,
-            col_names
+        logger.info(
+            "Train-test intersection {:.2f} %"
+            .format(
+             100 * len((set(train_df.msno.unique()) & set(test_df.msno.unique()))) / len(set(train_df.msno.unique()))
+            )
         )
 
+        encoders = create_encoders(
+            train_df, test_df, col_names
+        )
+
+        logger.info("Preparing raw data...")
         data_preparator = DataPreparator(
-            train_df, 
-            'msno', 
-            'song_id', 
-            encoders, 
-            test_set_rate=0.2
+            train_df, 'msno', 'song_id', encoders, test_set_rate=0.2
         )
-
+        del train_df
+        gc.collect()
         data_preparator.prepare_data()
 
-        logger.info("Feature columns for encoding: {}".format(col_names))
-
-        logger.info("Encoding train data...")
+        logger.info("Feature columns for encoding: {} \nEncoding train data...".format(col_names))
         feature_coded_train = FeatureEncoder(
-            data_preparator.train_set, 
-            col_names,
-            encoders,
-            num_cols,
-            amplifying_coef = AMP_COEFF
+            data_preparator.train_set, col_names, encoders,
+            matrix_name=config.TRAIN_MATRIX, dump_name=config.TRAIN_MATRIX_GZ
         )
         feature_coded_train.build()
-        logger.info("Dumping train matrix...")
-        io.mmwrite('../input/train.mtx', feature_coded_train.feature_matrix)
         train_matrix = feature_coded_train.feature_matrix
 
         logger.info("Encoding valid data...")
         feature_coded_valid = FeatureEncoder(
-            data_preparator.valid_set, 
-            col_names,
-            encoders,
-            num_cols,
-            amplifying_coef = AMP_COEFF
+            data_preparator.valid_set, col_names, encoders,
+            matrix_name=config.VALID_MATRIX, dump_name=config.VALID_MATRIX_GZ
         )
         feature_coded_valid.build()
-        logger.info("Dumping valid matrix...")
-        io.mmwrite('../input/valid.mtx', feature_coded_valid.feature_matrix)
         valid_matrix = feature_coded_valid.feature_matrix
 
-        logger.info("Dumping matrices...")
-        pickle.dump(data_preparator.train_set.target.values, open('../input/train_target.pkl', "wb"), protocol=3)
-        data_preparator.train_set.to_csv('../input/train_df.csv', index='False')
-        data_preparator.valid_set.to_csv('../input/valid_df.csv', index='False')
-        pickle.dump(data_preparator.valid_set.target.values, open('../input/valid_target.pkl', "wb"), protocol=3)
-        pickle.dump(col_names, open('../input/col_names.pkl', "wb"), protocol=3)
         train_target_values = data_preparator.train_set.target.values
         valid_target_values = data_preparator.valid_set.target.values
 
     else:
         logger.info('Загружаем test_df и train_df...')
-        train_df = pd.read_csv('../input/train_df.csv', index='False')
-        valid_df = pd.read_csv('../input/valid_df.csv', index='False')
+        train_df = pd.read_csv(config.TRAIN_DF_GZ, compression='gzip')
+        valid_df = pd.read_csv(config.VALID_DF_GZ, compression='gzip')
         logger.info('Загружаем матрицу test...')
-        valid_matrix = io.mmread('../input/valid.mtx')
+        valid_matrix = io.mmread(gzip.open(config.VALID_MATRIX_GZ, 'rb'))
         logger.info('Загружаем матрицу train...')
-        train_matrix = io.mmread('../input/train.mtx')
-        train_target_values = pickle.load(open('../input/train_target.pkl', 'rb'))
-        valid_target_values = pickle.load(open('../input/valid_target.pkl', 'rb'))
-        col_names = pickle.load(open('../input/col_names.pkl', 'rb'))
+        train_matrix = io.mmread(gzip.open(config.TRAIN_MATRIX_GZ, 'rb'))
+        train_target_values = pickle.load(open(config.TRAIN_TARGET, 'rb'))
+        valid_target_values = pickle.load(open(config.VALID_TARGET, 'rb'))
+        col_names = pickle.load(open(config.COL_NAMES, 'rb'))
+        encoders = pickle.load(open(config.ENCODERS, 'rb'))
         logger.info('Матрицы загружены!')
 
-    test_df = pd.read_csv('../input/test.csv') if test_df is None else test_df
-
     logger.info('Train matrix shape={}, nnz={}, sparsity^(-1)={}'.format(
-        train_matrix.shape, 
-        train_matrix.nnz,
+        train_matrix.shape, train_matrix.nnz,
         (train_matrix.shape[0]*train_matrix.shape[1])/train_matrix.nnz)
     )
 
-    y_train = np.array([-1 if i==0 else 1 for i in train_target_values])
+    y_train = np.array([-1 if i == 0 else 1 for i in train_target_values])
     y_train.min()
 
-    rank = [2, 4, 8, 10, 12, 16, 24, 30, 32, 40, 50, 60, 64, 128]
+    rank = [2, 4, 8, 10, 12, 16, 24, 30, 32, 40, 50, 60, 64]
 
     best_score = 0
     best_model = None
@@ -203,25 +211,20 @@ if __name__=='__main__':
         score = roc_auc_score(valid_target_values, y_hat)
         best_model = model if score > best_score else best_model
 
-        logger.info("r={}\tROC AUC = {}".format(r, score ))
+        logger.info("r={}\tROC AUC = {}".format(r, score))
 
-
-    logger.info('Сохраняем обученную модель...')
-    pickle.dump(best_model, open('../input/best_model.pkl', "wb"), protocol=3)
+    logger.info('Сохраняем обученную модель (лучшую)...')
+    pickle.dump(best_model, open(config.FM_MODEL, "wb"), protocol=3)
 
     logger.info('Building test set...')
+    test_df = pd.read_csv(config.TEST_CSV_GZ, compression='gzip') if test_df is None else test_df
     test_data = FeatureEncoder(
-        test_df, 
-        col_names,
-        encoders,
-        num_cols,
-        amplifying_coef = AMP_COEFF,
-        index_col = 'id'
+        test_df, col_names, encoders, index_col='id', is_dump=False
     )
-
     test_data.build()
+
     logger.info('Making predictions...')
     y_predict = best_model.predict_proba(test_data.feature_matrix)
-    result = pd.DataFrame({'id': test_df.id.values,'target': y_predict})
-    result.to_csv('../input/submit.csv', index=False, sep=',', compression = 'gzip', float_format = '%.5f')
+    result = pd.DataFrame({'id': test_df.id.values, 'target': y_predict})
+    result.to_csv(config.FM_SUBMIT, index=False, sep=',', compression='gzip', float_format='%.5f')
     logger.info('Done!')
